bcc6876ad9f47169ded5296379c288e91c8f39af
Jusmicelangelo <jusmicelangelo@Jusmicelangelos-MacBook-Pro.local>
2023-12-07 16:33:22 +0100

    final files

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,23 @@
-function findRotationCount() {
+function findRotationCount(arr, low = 0, high = arr.length - 1) {
+    if (high < low) return 0;
+    if (high === low) return low;
+    let mid = Math.floor((low + high) / 2)
   
-}
+    // is mid+1 minimum element
+    if (mid < high && arr[mid + 1] < arr[mid])
+      return mid + 1;
+  
+    // is mid minimum element
+    if (mid > low && arr[mid] < arr[mid - 1]) {
+      return mid;
+    }
+  
+    // direction right or left
+    if (arr[high] > arr[mid]) {
+      return findRotationCount(arr, low, mid - 1);
+    }
+  
+    return findRotationCount(arr, mid + 1, high);
+  }
 
 module.exports = findRotationCount
